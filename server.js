9a157f25c07e2c25827dfd0d081b2cb89df48198
gena.kim <kim.gena87@gmail.com>
2017-12-12 07:39:42 +0500

    Added Project Page

--- server.js
+++ server.js
@@ -64,6 +64,13 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/project', (req, res) => {
+    //res.send('<h1>About page</h1>');
+    res.render('project.hbs', {
+        pageTitle: 'Project Page',
+    });
+});
+
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'Unable to handle request'
